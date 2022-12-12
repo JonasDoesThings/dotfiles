44111080ef7908ff792422e6257c6fb3a68891cf
k4ds3 <5180118+k4ds3@users.noreply.github.com>
2022-12-12 17:15:03 +0100

    fix: use correct git repo in dlTheme.sh

--- dlTheme.sh
+++ dlTheme.sh
@@ -1,1 +1,1 @@
-git clone -c core.autocrlf=false https://github.com/agnoster/agnoster-zsh-theme "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/agnoster-zsh-theme"
+git clone -c core.autocrlf=false https://github.com/romkatv/powerlevel10k "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
